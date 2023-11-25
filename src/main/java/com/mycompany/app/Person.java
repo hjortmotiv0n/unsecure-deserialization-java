9fa5b5545011a391cb427a5525ca70e9ee9ce08b
hjortmotiv0n <142341384+hjortmotiv0n@users.noreply.github.com>
2023-11-25 20:58:26 +0100

    Upload of files

---
+++ src/main/java/com/mycompany/app/Person.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import java.io.Serializable;
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
+    // Getters and setters (if needed)
+}
---
+++ src/main/java/com/mycompany/app/SerializationExample.java
@@ -0,0 +1,32 @@
+package com.mycompany.app;
+
+import java.io.*;
+public class SerializationExample {
+    private byte[] serialize(Object object) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        try (ObjectOutputStream stream = new ObjectOutputStream(buffer)) {
+            stream.writeObject(object);
+        }
+        return buffer.toByteArray();
+    }
+    private Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream buffer = new ByteArrayInputStream(bytes);
+        try (ObjectInputStream stream = new ObjectInputStream(buffer)) {
+            return stream.readObject();
+        }
+    }
+    public static void main(String[] args) {
+        SerializationExample example = new SerializationExample();
+        Person person = new Person("Bob", 30);
+        try {
+            // Serialize the person object to a byte array
+            byte[] serializedData = example.serialize(person);
+            System.out.println("Serialized data: " + new String(serializedData));
+            // Deserialize the byte array back to a person object
+            Person deserializedPerson = (Person) example.deserialize(serializedData);
+            System.out.println("Deserialized Person: " + deserializedPerson);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
